74016315ae7e56aa6475caabc29f513a20330bb7
Lisa Hoeg <lisahoeg@gmail.com>
2022-06-09 15:42:30 -0400

    updated ex 2 to include ex 3

--- Exercises1/ex2.R
+++ Exercises1/ex2.R
@@ -1,16 +1,19 @@
 #Multiply the slider input by 5
+#Ex 3 extends this to have second multiplier slider instead of multiply by 5
+
 library(shiny)
 
 ui <- fluidPage(
   sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
-  "then x times 5 is",
+  sliderInput("y", label = "and y is", min = 1, max = 50, value = 5),
+  "then x times y is",
   textOutput("product")
 )
 
 server <- function(input, output, session) {
 
   output$product <- renderText({
-    input$x*5
+    input$x*input$y
   })
 }
 
